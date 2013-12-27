9232d678766f974cd1341fec8003eed60ccd0d18
Justin DeMaris <justin.demaris@buddymedia.com>
2013-12-27 14:45:01 -0500

    Working on the sql structure

---
+++ app/Resources/db/mysql.sql
@@ -0,0 +1,22 @@
+CREATE  TABLE `realestate`.`property` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `address1` VARCHAR(1024) NOT NULL ,
+  `address2` VARCHAR(1024) NULL ,
+  `city` VARCHAR(128) NOT NULL ,
+  `state` VARCHAR(45) NOT NULL ,
+  `zip` VARCHAR(32) NOT NULL ,
+  `price` INT(11) NULL ,
+  `rent_zestimate` INT(11) NULL ,
+  `rent_zestimate_low` INT(11) NULL ,
+  `rent_zestimate_high` INT(11) NULL ,
+  `zestimate_low` INT(11) NULL ,
+  `zestimate_high` INT(11) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+CREATE TABLE zipcodes
+(
+  zipcode VARCHAR(5) NOT NULL,
+  last_imported DATETIME
+);
+CREATE UNIQUE INDEX unique_zipcode ON zipcodes ( zipcode );
